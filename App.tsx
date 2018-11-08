38fec30c338bbae176b48872e3b5abf87e74c9d2
christopher p. k. morris <slime@christohersmini.hq.k.grp>
2018-11-08 15:55:32 +0000

    original hello-world for typescript working

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@
 import React from 'react';
 import {Component} from 'react';  
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Hello } from './components/Hello';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -25,6 +26,7 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Hello name="world" enthusiasmLevel={5}/>
       </View>
     );
   }
