8256e70076db6370300cffc03fc3d96b86d7af30
christopher p. k. morris <slime@christohersmini.hq.k.grp>
2018-11-12 17:35:26 +0000

    added api request

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@
 
 import React from 'react';
 import {Component} from 'react';  
-import {Platform, StyleSheet, Text, View, Dimensions, TouchableNativeFeedback, RippleBackgroundPropType, ThemeAttributeBackgroundPropType, Alert, GestureResponderEvent, ScrollView} from 'react-native';
+import {Platform, StyleSheet, Text, View, Dimensions, TouchableNativeFeedback, RippleBackgroundPropType, ThemeAttributeBackgroundPropType, Alert, GestureResponderEvent, ScrollView, FlatList, SectionList} from 'react-native';
 import { Hello } from './components/Hello';
 import hexGenerator from './utils/hexGenerator';
 import { Image } from 'react-native'
@@ -26,7 +26,19 @@ interface Props {};
 
 interface State {
   backgroundColour: string; 
+  movies: string[]
 }; 
+
+function getMoviesFromApiAsync() {
+  return fetch('https://facebook.github.io/react-native/movies.json')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      return responseJson.movies;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
 type androidNativeButton = RippleBackgroundPropType | ThemeAttributeBackgroundPropType | undefined; 
 
@@ -36,7 +48,8 @@ export default class App extends Component<Props, State> {
     super(props); 
 
     this.state = {
-      backgroundColour: hexGenerator()
+      backgroundColour: hexGenerator(),
+      movies: []
     }
   }
 
@@ -51,6 +64,7 @@ export default class App extends Component<Props, State> {
   
 
   componentDidMount(){
+    getMoviesFromApiAsync(); 
     window.setInterval(()=>{
       this.setState({backgroundColour: hexGenerator()}); 
     }, 200)
@@ -77,7 +91,7 @@ export default class App extends Component<Props, State> {
         <View style={{width: 50, flex: 2, backgroundColor: 'steelblue'}} />
 
 
-        <ScrollView pagingEnabled={true} style={{height:200, borderWidth: 5}}> 
+        <ScrollView pagingEnabled={true} style={{height:50, borderWidth: 5}}> 
           <Text> hello </Text>       
           <Text> hello </Text>
           <Text> hello </Text>
@@ -95,14 +109,40 @@ export default class App extends Component<Props, State> {
        
         {/* <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} /> */}
 
-        <TouchableNativeFeedback
-            onPress={this._onPressButton}
-            onLongPress={this._onLongPressButton}
-            background={(Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : '') as androidNativeButton}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableNativeFeedback</Text>
-          </View>
-        </TouchableNativeFeedback>
+        <SectionList style={{height: 50}}
+          sections={[
+            {title: 'D', data: ['Devin']},
+            {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+          keyExtractor={(item: any, index: number) => index.toString()}
+        />
+
+        <View style={{flex: 4, flexDirection: "row", borderColor: "green", borderWidth: 4}}>  
+          <TouchableNativeFeedback
+              onPress={this._onPressButton}
+              onLongPress={this._onLongPressButton}
+              background={(Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : '') as androidNativeButton}>
+            <View style={styles.button}>
+              <Text style={styles.buttonText}>TouchableNativeFeedback</Text>
+            </View>
+          </TouchableNativeFeedback>
+
+          <FlatList style={{ borderWidth: 5}}
+          data={[
+            {key: 'Devin'},
+            {key: 'Jackson'},
+            {key: 'James'},
+            {key: 'Joel'},
+            {key: 'John'},
+            {key: 'Jillian'},
+            {key: 'Jimmy'},
+            {key: 'Julie'},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
+        </View>
       </View>
     );
   }
@@ -134,5 +174,21 @@ const styles = StyleSheet.create({
   buttonText: {
     padding: 20,
     color: 'white'
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+    flex: 1
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: '#888',
+    color: "#eee"
   }
 });
